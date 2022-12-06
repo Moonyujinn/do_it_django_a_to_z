091e021503b874453fcd9ff7d749fd2da7aa0d24
tobagi0418 <tobag0418@naver.com>
2022-12-06 11:47:06 +0900

    포스트를 수정할 때 텍스트로 태그 지정하기

--- blog/tests.py
+++ blog/tests.py
@@ -255,12 +255,17 @@ class TestView(TestCase) :
             main_area = soup.find('div', id='main-area')
             self.assertIn('Edit Post', main_area.text)
 
+            tag_str_input = main_area.find('input', id='id_tags_str')
+            self.assertTrue(tag_str_input)
+            self.assertIn('파이썬 공부; python', tag_str_input.attrs['value'])
+
             response - self.client.post(
                 update_post_url,
                 {
                     'title': '세 번째 포스트를 수정했습니다.',
                     'content': '안녕 세계? 우리는 하나!',
-                    'category': self.category_music.pk
+                    'category': self.category_music.pk,
+                    'tags_str': '파이썬 공부; 한글 태그, some tag'
                 },
                 follow=True
             )
@@ -269,3 +274,4 @@ class TestView(TestCase) :
             self.assertIn('세 번쩨 포스트를 수정했습니다.', main_area.text)
             self.assertIn('안녕 세계? 우리는 하나!', main_area.text)
             self.assertIn(self.category_music.name, main_area.text)
+            self.assertIn('파이썬 공부', main_area.text)
