f2c4add3e76326d701d38aa03a7091043710a61a
tobagi0418 <tobag0418@naver.com>
2022-12-06 12:08:35 +0900

    django-crispy-forms 적용하기

--- blog/views.py
+++ blog/views.py
@@ -113,6 +113,26 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
         else:
             raise PermissionDenied
 
+    def form_valid(self, form):
+        response = super(PostUpdate, self).form_valid(form)
+        self.object.tags.clear()
+
+        tags_str = self.request.POST.get('tags_str')
+        if tags_str:
+            tags_str = tags_str.strip()
+            tags_str = tags_str.replace(',', ';')
+            tags_list = tags_str.split(';')
+
+            for t in tags_list:
+                t = t.strip()
+                tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                if is_tag_created:
+                    tag.slug = slugify(t, allow_unicode=True)
+                    tag.save()
+                self.object.tags.add(tag)
+
+        return response
+
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
 
