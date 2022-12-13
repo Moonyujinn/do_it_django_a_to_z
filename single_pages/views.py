ac9586fe03be64d1b86da5d069a9daf53e82f987
tobagi0418 <tobag0418@naver.com>
2022-12-13 11:03:15 +0900

    대문 페이지 수정하기

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from blog.models import Post
 
 def landing(request):
+    recent_post = Post.objects.order_by('-pk')[:3]
     return render(
         request,
-        'single_pages/landing.html'
+        'single_pages/landing.html',
+        {
+            'recent_posts': recent_post,
+        }
     )
 
 def about_me(request):
